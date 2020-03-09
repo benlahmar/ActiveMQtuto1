429197a8203ea91cc4b0a2f39ebd9a2d12d7e74d
benlahmar <h.benlahmer@gmail.com>
2020-03-09 12:29:16 +0000

    add

---
+++ activemqtp-2/src/main/java/com/app/moi/Activemqtp2Application.java
@@ -0,0 +1,50 @@
+package com.app.moi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import javax.jms.ConnectionFactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.config.JmsListenerContainerFactory;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+import com.app.moi.md.Email;
+@SpringBootApplication
+@EnableJms
+public class Activemqtp2Application {
+
+	public static void main(String[] args) {
+		 ConfigurableApplicationContext context = SpringApplication.run(Activemqtp2Application.class, args);
+		 JmsTemplate jmsTemplate = context.getBean(JmsTemplate.class);
+
+		 System.out.println("Sending an email message.");
+		    jmsTemplate.convertAndSend("mailbox", new Email("info@example.com", "Hello"));
+	}
+	
+	
+	 @Bean
+	  public JmsListenerContainerFactory<?> myFactory(ConnectionFactory connectionFactory,
+	     DefaultJmsListenerContainerFactoryConfigurer configurer) {
+	    DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+	    configurer.configure(factory, connectionFactory);
+	    return factory;
+	  }
+
+	  @Bean // Serialize message content to json using TextMessage
+	  public MessageConverter jacksonJmsMessageConverter() {
+	    MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+	    converter.setTargetType(MessageType.TEXT);
+	    converter.setTypeIdPropertyName("_type");
+	    return converter;
+	  }
+
+}
---
+++ activemqtp-2/src/main/java/com/app/moi/md/Receiver.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.app.moi.md;
+
+/**
+ * @author BEN LAHMAR EL HABIB
+ *
+ */
+
+
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Receiver {
+
+  @JmsListener(destination = "mailbox", containerFactory = "myFactory")
+  public void receiveMessage(Email email) {
+    System.out.println("Received <" + email + ">");
+  }
+
+}
